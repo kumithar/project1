0d973bd4d58efd073426a0e2495f2994f56c53b6
Igor <kumithar@gmail.com>
2022-02-22 17:27:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* alert('Hello'); */
+/* const result = confirm('Are you here?');
+console.log(result); */
+
+/* const answer = prompt('Вам есть 18?', '18');
+console.log(typeof(answer)); */
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.png'];
+console.log(arr[1]); */
